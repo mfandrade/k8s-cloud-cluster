8f3ae1476b085018d62f2695c25bb972e3b1a922
Marcelo F Andrade <mfandrade@gmail.com>
2022-10-02 13:11:14 -0300

    lock versions

--- 1_terraform/main.tf
+++ 1_terraform/main.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_version = "1.3.1"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.33.0"
+    }
+  }
   backend "s3" {
     region  = "us-east-1"
     bucket  = "clusterk8s-backend"
